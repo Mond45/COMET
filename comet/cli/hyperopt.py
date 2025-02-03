d53c8d6e7c07a615fa8f88967e5d71a7b86d98e8
Mond45 <mondc45@gmail.com>
2025-02-03 14:47:06 +0700

    separate train and hyperparameter optimize script

---
+++ comet/cli/hyperopt.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+from functools import partial
+import warnings
+
+import optuna
+from comet.cli.train import initialize_model, initialize_trainer, read_arguments
+from pytorch_lightning import seed_everything
+
+
+def optuna_objective(trial: optuna.trial.Trial, cfg):
+    cfg = deepcopy(cfg)
+    cfg.unified_metric.init_args.learning_rate = trial.suggest_float(
+        "learning_rate", 1e-6, 1e-4, log=True
+    )
+    trainer = initialize_trainer(cfg)
+    model = initialize_model(cfg)
+    trainer.fit(model)
+    return trainer.early_stopping_callback.best_score.item()
+
+
+def hyperopt():
+    parser = read_arguments()
+    cfg = parser.parse_args()
+
+    seed_everything(cfg.seed_everything)
+
+    warnings.filterwarnings(
+        "ignore",
+        category=UserWarning,
+        message=".*Consider increasing the value of the `num_workers` argument` .*",
+    )
+
+    study = optuna.create_study(direction="maximize")
+    study.optimize(partial(optuna_objective, cfg=cfg), n_trials=10)
+    print(study.best_params)
+
+
+if __name__ == "__main__":
+    hyperopt()
